c98ac73962b98972b5b1bd447e5abcb62138ad5e
James Todd <toddwebnet@gmail.com>
2018-06-09 18:00:22 -0500

    update

---
+++ app/Console/Commands/MonitorCommand.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Console\Commands;
+
+use App\Services\NetworkService;
+use Illuminate\Console\Command;
+
+class MonitorCommand extends Command
+{
+
+    protected $signature = 'util:monitor';
+    protected $description = "Monitor the stuff";
+
+    public function handle()
+    {
+        for ($x = 1; $x < 255; $x++) {
+            $ip = "192.168.11.{$x}";
+            print $ip . " - ";
+            $ping = NetworkService::ping($ip);
+            if ($ping == 0) {
+                print "down\n";
+            } else {
+                print "up\n";
+            }
+        }
+        
+    }
+}
