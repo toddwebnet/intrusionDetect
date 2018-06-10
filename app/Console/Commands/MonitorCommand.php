7b94938dfe41d4fca3aac2863c42a0095be74d09
James Todd <toddwebnet@gmail.com>
2018-06-10 13:17:15 -0500

    update

--- app/Console/Commands/MonitorCommand.php
+++ app/Console/Commands/MonitorCommand.php
@@ -15,53 +15,49 @@ class MonitorCommand extends Command
 
     public function handle()
     {
-        $this->leaveIfAlreadyRunning();
-
-        while (true) {
-            foreach (NetworkService::arpScan() as $mac => $data) {
-                IpLoggingService::logIp($mac, $data);
-            }
-            $this->checkForIntrusions();
-            sleep(60);
+        foreach (NetworkService::arpScan() as $mac => $data) {
+            IpLoggingService::logIp($mac, $data);
         }
+        $this->checkForIntrusions();
     }
 
-    private function leaveIfAlreadyRunning()
-    {
-        if(file_exists('/tmp/artisan_util_monitor')){
-            print "leaving";
-            exit();
-        }
-        $myPid = getmypid();
-        file_put_contents('/tmp/artisan_util_monitor', $myPid);
-        $cmd = 'ps -aux | grep artisan';
-        $results = [];
-
-        foreach (NetworkService::runCmd($cmd) as $line) {
-            print $line . "\n";
-            $flag = false;
-            if (preg_match("/php/i", $line)) {
-                $pid = null;
-                foreach (explode(" ", $line) as $proc) {
-                    if (!is_numeric($pid) && is_numeric($proc)) {
-                        $pid = (int)$proc;
-                    }
-                    if ($proc == 'util:monitor') {
-                        $flag = true;
-                    }
-                }
-            }
-            if ($flag && $myPid != $pid) {
-                dump([
-                    $line,
-                    $pid
-                ]);
-                print "\nleaving\n";
+    /*
+        private function leaveIfAlreadyRunning()
+        {
+            if (file_exists('/tmp/artisan_util_monitor')) {
+                print "leaving";
                 exit();
             }
-        }
-    }
+            $myPid = getmypid();
+            file_put_contents('/tmp/artisan_util_monitor', $myPid);
+            $cmd = 'ps -aux | grep artisan';
+            $results = [];
 
+            foreach (NetworkService::runCmd($cmd) as $line) {
+                print $line . "\n";
+                $flag = false;
+                if (preg_match("/php/i", $line)) {
+                    $pid = null;
+                    foreach (explode(" ", $line) as $proc) {
+                        if (!is_numeric($pid) && is_numeric($proc)) {
+                            $pid = (int)$proc;
+                        }
+                        if ($proc == 'util:monitor') {
+                            $flag = true;
+                        }
+                    }
+                }
+                if ($flag && $myPid != $pid) {
+                    dump([
+                        $line,
+                        $pid
+                    ]);
+                    print "\nleaving\n";
+                    exit();
+                }
+            }
+        }
+    */
     private function checkForIntrusions()
     {
         foreach (IpLoggingService::getNewFiles() as $file) {
